9c95e174229cac2e9398ac84e9dbd315e7bc5d84
Aaron Chellam <aaron.chellam@outlook.com>
2023-12-12 14:08:51 +0000

    Added signup page testing

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,12 +6,21 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        # Add age to the existing, default UserCreationForm fields.
-        fields = UserCreationForm.Meta.fields + ("age",)
+        # # Add age to the existing, default UserCreationForm fields.
+        # fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 
 # Provides a form for admin users to edit user information.
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class SignpuPageTests(TestCase):
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@email.com",
+                "password1": "testpass123",
+                "password2": "testpass123"
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+        # Confirm there is one user in the database.
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+
+        # Verify the username and email of the user just added to the test database.
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser@email.com")
