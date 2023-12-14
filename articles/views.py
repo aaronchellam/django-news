e790916a01c0484a3f9612ebfd90ec4e3ff8a142
Aaron Chellam <aaron.chellam@outlook.com>
2023-12-14 17:11:24 +0000

    Added article creation with automatic author authorisation

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
 from .models import Article
 
@@ -29,3 +29,21 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = (
+        'title',
+        'body',
+        # 'author'
+    )
+
+    # New articles' authors should default to the logged in user.
+    # form_valid is called when the submitted form is valid.
+    def form_valid(self, form):
+        # Override the default behaviour to set the author of the new article to the currently logged in user.
+        form.instance.author = self.request.user
+        # Call the parent class's 'form_valid' method
+        return super().form_valid(form)
